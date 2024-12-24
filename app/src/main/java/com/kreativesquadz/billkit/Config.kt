7dac4913412fa7bafa4715a0a94e5e0bbf184aaf
BemiXplorer <imran.shaikh1483@gmail.com>
2024-12-24 15:41:14 +0530

    billbaaz setup

--- app/src/main/java/com/kreativesquadz/billkit/Config.kt
+++ app/src/main/java/com/kreativesquadz/billkit/Config.kt
@@ -3,8 +3,8 @@ package com.kreativesquadz.billkit
 class Config {
     companion object {
         var APP_VERSION: String = "1.0.0"
-      const val APP_API_URL: String = "https://billkit.kreativesquadz.com/"
-      const val APP_API_IMAGE_URL: String = "https://billkit.kreativesquadz.com/src/uploads/"
+      const val APP_API_URL: String = "https://billbaaz.kreativesquadz.com/"
+      const val APP_API_IMAGE_URL: String = "https://billbaaz.kreativesquadz.com/src/uploads/"
       const val API_Key: String = "BillKitApi_v_1"
       const val  ERROR_CODE_10001: Int = 10001 // Totally No Record
       const val CURRENCY: String = "RS "
